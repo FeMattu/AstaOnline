16c4614efc2a5274c667aed41ed888ccc5a0e353
Utente <Utente@Pc-di-Bobo>
2023-04-24 12:56:42 +0200

    Output visivo migliorato, stampa a schermo delle stringhe rappresentanti successi/insuccessi di connessioni e accessi ad aste, creato loop in client e server dove cicla ogni volta che si vuol inserire un prodotto, in maniera tale che un utente subito dopo che ha inserito un prodotto possa anche partecipare ad un asta o disconnettersi (se sceglie di partecipare ad un asta o di disconnettersi, il programma non riesegue il loop)

--- Asta/src/Connection/Client.java
+++ Asta/src/Connection/Client.java
@@ -11,15 +11,16 @@ import gambit.Resources;
 
 /**
  * <b>Client class</b>
+ * 
  * @author <i>Federico Mattucci<br>
- * 			  Tommaso Giannecchini<br>
- * 			  Federico Massanti<br>
- * 			  Lorenzo Rapposelli<br>
- * 			  Giacomo Diridoni</i>
+ *         Tommaso Giannecchini<br>
+ *         Federico Massanti<br>
+ *         Lorenzo Rapposelli<br>
+ *         Giacomo Diridoni</i>
  *
  */
 public class Client {
-	
+
 	private static Socket socket;
 	private static BufferedReader reader;
 	private static DataOutputStream writer;
@@ -27,7 +28,7 @@ public class Client {
 	private static String username;
 	private static String password;
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		try {
 			socket = new Socket("localhost", 5234);
 			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -35,11 +36,11 @@ public class Client {
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
-		
+		}
+
 		System.out.println("Connessione al server...");
-		//Se non viene effettuato l'accesso al server il programma termina
-		if(!accessoServer()){
+		// Se non viene effettuato l'accesso al server il programma termina
+		if (!accessoServer()) {
 			try {
 				System.out.println("Connessione al server non effettuata");
 				reader.close();
@@ -50,50 +51,56 @@ public class Client {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			return; 
+			return;
 		}
-		
+
 		System.out.println("Accesso effettuato");
-		
-		System.out.println("---\n---\n---");
-		int scelta = sceltaMenu();
-		
-		try {
-			writer.writeBytes(scelta+"\n");
-			switch (scelta) {
-			case 1:
-				//Scegliendo questa, passiamo alla scelta dell'asta alla quale prendere parte
-				partecipaAdUnAsta();
-				break;
-			case 2:
-				//Scegliendo questa, inseriamo un prodotto che verrà messo all'asta
-				aggiuntaProdotto();
-				break;
-			case 0:
-				break;
-			default:
-				System.out.println("Scelta non valida, reinserire.");
-				break;
+		int sceltaMenu = -1;
+		do {
+			System.out.println("---\n---\n---");
+			sceltaMenu = sceltaMenu();
+
+			try {
+				writer.writeBytes(sceltaMenu + "\n");
+				switch (sceltaMenu) {
+				case 1:
+					// Scegliendo questa, passiamo alla scelta dell'asta alla quale prendere parte
+					partecipaAdUnAsta();
+					System.out.println(reader.readLine());
+					break;
+				case 2:
+					// Scegliendo questa, inseriamo un prodotto che verrà messo all'asta
+					aggiuntaProdotto();
+					break;
+				case 0:
+					System.out.println("Termine del programma. Disconnessione in corso...");
+					reader.close();
+					writer.close();
+					socket.close();
+					System.out.println("Disconnessione eseguita con successo.");
+					break;
+				default:
+					System.out.println("Scelta non valida, reinserire.");
+					break;
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			
-			reader.close();
-			writer.close();
-			socket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	
-		
+		} while (sceltaMenu == 2);
+
 		scanner.close();
 	}
-	
+
 	/**
 	 * permette al client di accedere o registrarsi alla "piattaforma"
-	 * @return <strong>true</strong> se l'accesso è stato effettuato, <strong>false</strong> altrimenti
+	 * 
+	 * @return <strong>true</strong> se l'accesso è stato effettuato,
+	 *         <strong>false</strong> altrimenti
 	 */
 	private static boolean accessoServer() {
 		int scelta = -1;
-		while(true) {
+		while (true) {
 			System.out.println("1 -> Accedi");
 			System.out.println("2 -> registrati");
 			System.out.println("0 -> exit");
@@ -105,51 +112,52 @@ public class Client {
 				username = scanner.next();
 				System.out.print("Password: ");
 				password = scanner.next();
-				
+
 				try {
-					writer.writeBytes(username+":"+password+"\n");
-					if(reader.readLine().equals("OK")) {
+					writer.writeBytes(username + ":" + password + "\n");
+					if (reader.readLine().equals("OK")) {
 						return true;
-					}else {
+					} else {
 						System.out.println("Username o psw non corretti, riprovare");
 					}
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}	
+				}
 				break;
 			}
 			case 2: {
 				String nome, cognome, residenza, email = null;
 				System.out.print("Username: ");
 				username = scanner.next();
-				
+
 				System.out.print("Nome: ");
 				nome = scanner.next();
-				
+
 				System.out.print("Cognome: ");
 				cognome = scanner.next();
-				
+
 				System.out.print("E-mail: ");
 				email = scanner.next();
 				scanner.nextLine();
 				System.out.print("Residenza: ");
 				residenza = scanner.nextLine();
-				
+
 				System.out.print("Password (senza spazi): ");
 				password = scanner.next();
-				
+
 				try {
-					writer.writeBytes(username+":"+password+":"+nome+":"+cognome+":"+email+":"+residenza+"\n");
-					if(reader.readLine().equals("OK")) {
+					writer.writeBytes(username + ":" + password + ":" + nome + ":" + cognome + ":" + email + ":"
+							+ residenza + "\n");
+					if (reader.readLine().equals("OK")) {
 						return true;
-					}else {
+					} else {
 						System.out.println("Registrazione fallita, riprovare");
 					}
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}				
+				}
 				break;
 			}
 			case 0: {
@@ -158,58 +166,58 @@ public class Client {
 			default:
 				System.out.println("Scelta immessa non valida, reinserire");
 			}
-		}	
+		}
 	}
-	
-	private static void menu(){
-		System.out.println("\nMenù:");
+
+	private static void menu() {
+		System.out.println("Menù:");
 		System.out.println("1 -> Partecipa ad un'asta");
 		System.out.println("2 -> Aggiungi un prodotto");
 		System.out.println("0 -> Esci");
 		System.out.print("Scelta: ");
 	}
-	
+
 	private static int sceltaMenu() {
 		menu();
 		int s = scanner.nextInt();
 		return s;
 	}
-	
+
 	private static void partecipaAdUnAsta() throws IOException {
 		System.out.println("---\nElenco aste a cui puoi partecipare:");
 		String asta = reader.readLine();
-		while(!asta.equals("OK")) {
+		while (!asta.equals("OK")) {
 			System.out.println(asta);
 			asta = reader.readLine();
 		}
 		System.out.print("A quale asta vuoi partecipare (ID)?: ");
 		int idAstaScelta = scanner.nextInt();
-		writer.writeBytes(idAstaScelta+"\n");
+		writer.writeBytes(idAstaScelta + "\n");
 	}
-	
+
 	private static void aggiuntaProdotto() throws IOException {
-		String nome,desc,categoria;
+		String nome, desc, categoria;
 		float prezDiBase;
 		System.out.println("---\nInserisci un prodotto:\n---");
-		
+
 		System.out.print("Nome: ");
 		scanner.nextLine();
 		nome = scanner.nextLine();
-		System.out.println(nome);
-		
+		// System.out.println(nome);
+
 		System.out.print("Descrizione: ");
 		desc = scanner.nextLine();
-		System.out.println(desc);
-		
+		// System.out.println(desc);
+
 		System.out.print("Categoria: ");
 		categoria = scanner.next();
-		System.out.println(categoria);
-		
+		// System.out.println(categoria);
+
 		System.out.print("Prezzo di Base: ");
 		prezDiBase = scanner.nextFloat();
-		System.out.println(prezDiBase);
-		
-		writer.writeBytes(nome+":"+desc+":"+categoria+":"+prezDiBase+"\n");
+		// System.out.println(prezDiBase);
+
+		writer.writeBytes(nome + ":" + desc + ":" + categoria + ":" + prezDiBase + "\n");
 		System.out.println(reader.readLine());
 	}
 
--- Asta/src/Connection/ServerTCPThread.java
+++ Asta/src/Connection/ServerTCPThread.java
@@ -18,154 +18,153 @@ import gambit.Resources;
 
 /**
  * <b>ServerTCPThread class</b>
+ * 
  * @author <i>Federico Mattucci<br>
- * 			  Tommaso Giannecchini<br>
- * 			  Federico Massanti<br>
- * 			  Lorenzo Rapposelli<br>
- * 			  Giacomo Diridoni</i>
+ *         Tommaso Giannecchini<br>
+ *         Federico Massanti<br>
+ *         Lorenzo Rapposelli<br>
+ *         Giacomo Diridoni</i>
  *
  */
-public class ServerTCPThread extends Thread{
-	
+public class ServerTCPThread extends Thread {
+
 	private Socket socket;
 	private BufferedReader reader;
 	private DataOutputStream writer;
 	private Resources resources;
 	static boolean inviato;
 	private Cliente utente;
-	
+
 	/**
 	 * <b>ServerTCPThread constructor</b>
-	 * @param socket -> socket used for the connection
-	 * @param resources -> resources istance 
+	 * 
+	 * @param socket    -> socket used for the connection
+	 * @param resources -> resources istance
 	 */
-    public ServerTCPThread(Socket socket, Resources resources){
-        this.socket=socket;
-        this.resources=resources;
-        try {
+	public ServerTCPThread(Socket socket, Resources resources) {
+		this.socket = socket;
+		this.resources = resources;
+		try {
 			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			writer = new DataOutputStream(socket.getOutputStream());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
-    
-    /**
-     * <b>Run method for ServerTCPThread class</b>
-     */
-    @Override
-    public void run() {
-    	// TODO Auto-generated method stub
-    	super.run();  
-    	String access;
+	}
+
+	/**
+	 * <b>Run method for ServerTCPThread class</b>
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+		String access;
 		try {
 			access = reader.readLine();
-			if(validazioneAccesso(access)) {
+			if (validazioneAccesso(access)) {
 				writer.writeBytes("OK\n");
-			}else {
+			} else {
 				writer.writeBytes("Nan\n");
 				reader.close();
 				writer.close();
 				socket.close();
 			}
-			
-			String choice = reader.readLine();
-			switch(choice){
-			case "1":
-				List<Asta> aste = resources.getCurrentGambits();
-				for(int i=0;i<aste.size();i++) {
-					writer.writeBytes(aste.get(i).toString()+"\n");
+			String choice = "";
+
+			do {
+				choice = reader.readLine();
+				switch (choice) {
+				case "1":
+					List<Asta> aste = resources.getCurrentGambits();
+					for (int i = 0; i < aste.size(); i++) {
+						writer.writeBytes(aste.get(i).toString() + "\n");
+					}
+					writer.writeBytes("OK\n");
+					// Da qui, gestione della scelta dell'asta alla quale si vuole partecipare
+					int astaScelta = Integer.parseInt(reader.readLine());
+					if (validazioneAstaScelta(astaScelta, aste)) {
+						System.out.println("Client ha accesso all'asta.");
+						writer.writeBytes("Hai effettuato l'accesso all'asta\n");
+					} else {
+						System.out.println("Client non ha effettuato accesso all'asta.");
+						writer.writeBytes("Non hai effettuato l'accesso all'asta\n");
+					}
+					break;
+				case "2":
+					String[] propProdotto = reader.readLine().split(":");
+					Prodotto p = new Prodotto(resources.getIdUltimoProdotto() + 1, propProdotto[0], propProdotto[1],
+							Float.parseFloat(propProdotto[3]), false, utente, Timestamp.valueOf(LocalDateTime.now()),
+							propProdotto[2]);
+					if (resources.addProdotto(p)) {
+						writer.writeBytes("Prodotto inserito\n");
+					} else {
+						writer.writeBytes("Prodotto non inserito\n");
+					}
+					break;
+				case "0":
+					System.out.println("Client non ha richiesto nulla.");
+					break;
+				default:
+					System.out.println("Scelta invalida, reinserisci.");
+					break;
 				}
-				writer.writeBytes("OK\n");
-				//Da qui, gestione della scelta dell'asta alla quale si vuole partecipare
-				int astaScelta = Integer.parseInt(reader.readLine());
-				if(validazioneAstaScelta(astaScelta, aste)) {
-					System.out.println("Client ha accesso all'asta.");
-				}
-				break;
-			case "2":
-				String[] propProdotto = reader.readLine().split(":");
-				Prodotto p = new Prodotto(resources.getIdUltimoProdotto()+1,
-										  propProdotto[0],
-										  propProdotto[1],
-										  Float.parseFloat(propProdotto[3]),
-										  false,
-										  utente,
-										  Timestamp.valueOf(LocalDateTime.now()),
-										  propProdotto[2]);
-				if(resources.addProdotto(p)) {
-					writer.writeBytes("Prodotto inserito\n");
-				}else {
-					writer.writeBytes("Prodotto non inserito\n");
-				}
-				break;
-			case "0":
-				break;
-			default:
-				System.out.println("Scelta invalida, reinserisci.");
-				break;
-			}
-			
+			} while (choice.equals("2"));
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-    }
-    
-    private boolean validazioneAccesso(String access) {
-    	String[] parametri = access.split(":");
-    	if(parametri.length == 2) {
-    		utente = resources.getCliente(parametri[0]);
-    		if(utente==null) return false;
-    		if(utente.getPassword().equals(parametri[1])) {
-    			return true;
-    		}
-    	}else if(parametri.length > 2){
-    		utente = new Cliente(
-    				parametri[0], 
-    				parametri[2], 
-    				parametri[3],
-    				Date.valueOf(LocalDate.now()),
-    				parametri[5],
-    				parametri[1], 
-    				parametri[4]);
-    		return resources.addCliente(utente);
-    	}
-    	
-    	return false;
-    }
-    
-    /**
-     * invia le informazioni sulle aste attive al client con il seguente formato:
-     * id_asta:nomeProdotto:categoria:prezzoCorrente;
-     * @return
-     */
-    private boolean sendAste() {
-    	List<Asta> currentGambits = resources.getCurrentGambits();
-    	try {
-    		for (Asta asta : currentGambits) {
-    			Prodotto p = asta.getProdotto();
-				writer.writeBytes(asta.getId_asta()+":"
-						+ p.getNome()+":"
-						+ p.getCategoria()+":"
+
+	}
+
+	private boolean validazioneAccesso(String access) {
+		String[] parametri = access.split(":");
+		if (parametri.length == 2) {
+			utente = resources.getCliente(parametri[0]);
+			if (utente == null)
+				return false;
+			if (utente.getPassword().equals(parametri[1])) {
+				return true;
+			}
+		} else if (parametri.length > 2) {
+			utente = new Cliente(parametri[0], parametri[2], parametri[3], Date.valueOf(LocalDate.now()), parametri[5],
+					parametri[1], parametri[4]);
+			return resources.addCliente(utente);
+		}
+
+		return false;
+	}
+
+	/**
+	 * invia le informazioni sulle aste attive al client con il seguente formato:
+	 * id_asta:nomeProdotto:categoria:prezzoCorrente;
+	 * 
+	 * @return
+	 */
+	private boolean sendAste() {
+		List<Asta> currentGambits = resources.getCurrentGambits();
+		try {
+			for (Asta asta : currentGambits) {
+				Prodotto p = asta.getProdotto();
+				writer.writeBytes(asta.getId_asta() + ":" + p.getNome() + ":" + p.getCategoria() + ":"
 						+ asta.getPrezzoCorrente());
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	return false;
-    }
-    
-    private boolean validazioneAstaScelta(int id, List<Asta> aste) {
-    	for(int i=0;i<aste.size();i++) {
-    		if(aste.get(i).getId_asta() == id) {
-    			return true;
-    		}
-    	}
 		return false;
 	}
-    
+
+	private boolean validazioneAstaScelta(int id, List<Asta> aste) {
+		for (int i = 0; i < aste.size(); i++) {
+			if (aste.get(i).getId_asta() == id) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
